2f2d2c72da3dafb77f2cdb729952fbc229ecba13
akapoor <anshul.kapoor@cern.ch>
2022-01-14 04:00:24 +0100

    Modular

--- getremoteskimcommand.sh
+++ getremoteskimcommand.sh
@@ -3,8 +3,9 @@
 sname="$1"
 outfolder="$2"
 remotefilelist="$3"
+analysis="$4"
 
 echo "Run the command below in terminal (as one command) to Skim $1 sample remotely! Output skims will be stored in $2. List of remote files will be taken from $3"
 echo "-----------------------------------"
-echo "maindir=$PWD;cd condor; a=0; while read -r line; do echo \$a; condor_submit job.sh -append \"arguments = cd $maindir && python Skimmer.py --name $sname --file root://xrootd-cms.infn.it//\${line} --saveroot --outfolder $outfolder --outsuffix $sname-\$a\"; let \"a += 1\"; done <$remotefilelist; cd .."
+echo "maindir=$PWD;cd condor; a=0; while read -r line; do echo \$a; condor_submit job.sh -append \"arguments = cd $maindir && python Skimmer-new.py --name $sname --file root://xrootd-cms.infn.it//\${line} --saveroot --outfolder $outfolder --debugprint --analysis $analysis --saveroot --outsuffix $sname-\$a\"; let \"a += 1\"; done <$remotefilelist; cd .."
 echo "-----------------------------------"
--- include/Helper.h
+++ include/Helper.h
@@ -1,4 +1,17 @@
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TLatex.h"
+#include "Math/Vector4D.h"
+#include "TStyle.h"
+ 
+using namespace ROOT;
+using namespace ROOT::VecOps;
+using RNode = ROOT::RDF::RNode;
+
 using Vec_t = const ROOT::RVec<float>&;
+using RVecF = RVec<float>;
 float ComputeInvariantMass(Vec_t pt, Vec_t eta, Vec_t phi, Vec_t mass) {
     const ROOT::Math::PtEtaPhiMVector p1(pt[0], eta[0], phi[0], mass[0]);
     const ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], mass[1]);
@@ -22,6 +35,25 @@ float DeltaR(float eta1,float phi1,float eta2,float phi2)
     return TMath::Sqrt(deltaEta*deltaEta + deltaPhi*deltaPhi);
 }
 
+
+RVecF Varbutindices(Vec_t var, Vec_t indices)
+{
+    
+    RVecF Varfromindices(2);
+    //Varfromindices[0]=1;
+    //Varfromindices[0]=2;
+    
+    for (size_t i = 0; i < indices.size(); i++) {
+        float ind=indices[i];
+        if (var.size()>i) Varfromindices[i]=var[ind];
+        else{ 
+            Varfromindices[i]=-999; //Protection for if there is no value for var at i
+            std::cout<<"Check issue in Varbutindices, size of variables not compatible"<<endl;
+        }
+    } 
+    return Varfromindices;
+}
+
 /*
 bool dr_cuts(Vec_t el_eta, Vec_t el_phi)
 {
@@ -35,54 +67,47 @@ bool dr_cuts(Vec_t el_eta, Vec_t el_phi)
 int ChargeFlip_Category(float MyElectron_pt1,float MyElectron_pt2,float MyElectron_eta1,float MyElectron_eta2)
 {
     int el1LMH,el2LMH,el1BE,el2BE;
-    el1LMH=1*(MyElectron_pt1<25) + 2*(MyElectron_pt1>=25 && MyElectron_pt1<50) + 3*(MyElectron_pt1>=50);
-    el2LMH=1*(MyElectron_pt2<25) + 2*((MyElectron_pt2>=25) && (MyElectron_pt2<50)) + 3*(MyElectron_pt2>=50);
-    
-    el1BE=1*(abs(MyElectron_eta1)<1.479) + 2*(abs(MyElectron_eta1)>=1.479);
-    el2BE=1*(abs(MyElectron_eta2)<1.479) + 2*(abs(MyElectron_eta2)>=1.479);
-    
-    
-    if (el1BE==1 && el1LMH==1 && el2BE==1 && el2LMH==1)
-        return 1;
-    if (el1BE==1 && el1LMH==2 && el2BE==1 && el2LMH==1)
-        return 2;
-    if (el1BE==1 && el1LMH==2 && el2BE==1 && el2LMH==2)
-        return 3;
-    if (el1BE==1 && el1LMH==3 && el2BE==1 && el2LMH==1)
-        return 4;
-    if (el1BE==1 && el1LMH==3 && el2BE==1 && el2LMH==2)
-        return 5;
-    if (el1BE==1 && el1LMH==3 && el2BE==1 && el2LMH==3)
-        return 6;
-    if (el1BE==2 && el1LMH==1 && el2BE==2 && el2LMH==1)
-        return 7;
-    if (el1BE==2 && el1LMH==2 && el2BE==2 && el2LMH==1)
-        return 8;
-    if (el1BE==2 && el1LMH==2 && el2BE==2 && el2LMH==2)
-        return 9;
-    if (el1BE==2 && el1LMH==3 && el2BE==2 && el2LMH==1)
-        return 10;
-    if (el1BE==2 && el1LMH==3 && el2BE==2 && el2LMH==2)
-        return 11;
-    if (el1BE==2 && el1LMH==3 && el2BE==2 && el2LMH==3)
-        return 12;
-    if (el1BE==1 && el1LMH==1 && el2BE==2 && el2LMH==1)
-        return 13;
-    if (el1BE==1 && el1LMH==2 && el2BE==2 && el2LMH==1)
-        return 14;
-    if (el1BE==2 && el1LMH==2 && el2BE==1 && el2LMH==1)
-        return 15;
-    if (el1BE==1 && el1LMH==2 && el2BE==2 && el2LMH==2)
-        return 16;
-    if (el1BE==1 && el1LMH==3 && el2BE==2 && el2LMH==1)
-        return 17;
-    if (el1BE==2 && el1LMH==3 && el2BE==1 && el2LMH==1)
-        return 18;
-    if (el1BE==1 && el1LMH==3 && el2BE==2 && el2LMH==2)
-        return 19;
-    if (el1BE==2 && el1LMH==3 && el2BE==1 && el2LMH==2)
-        return 20;
-    if (el1BE==1 && el1LMH==3 && el2BE==2 && el2LMH==3)
-        return 21;
+
+    el1LMH=2;
+    if (MyElectron_pt1<=25){el1LMH=1;}
+    if (MyElectron_pt1>=50){el1LMH=3;}
+
+    el2LMH=2;
+    if (MyElectron_pt2<=25){el2LMH=1;}
+    if (MyElectron_pt2>=50){el2LMH=3;}
+
+    el1BE=1;
+    if (abs(MyElectron_eta1)>=1.479){el1BE=2;}
+
+
+    el2BE=1;
+    if (abs(MyElectron_eta2)>=1.479){el2BE=2;}
+
+    int cat=el1BE*1000 + el1LMH*100 + el2BE*10 + el2LMH;
+
+
+    if (cat==1111) return 1;
+    if (cat==1211 || cat==1112) return 2;
+    if (cat==1212) return 3;
+    if (cat==1311 || cat==1113) return 4;
+    if (cat==1312 || cat==1213) return 5;
+    if (cat==1313) return 6;
+    if (cat==2121) return 7;
+    if (cat==2221 || cat==2122) return 8;
+    if (cat==2222) return 9;
+    if (cat==2321|| cat==2123) return 10;
+    if (cat==2322 || cat==2223) return 11;
+    if (cat==2323) return 12;
+
+    if (cat==1121 || cat==2111) return 13;
+    if (cat==1221 || cat==2112) return 14;
+    if (cat==2211 || cat==1122) return 15;
+    if (cat==1222 || cat==2212) return 16;
+    if (cat==1321 || cat==2113) return 17;
+    if (cat==2311 || cat==1123) return 18;
+    if (cat==1322 || cat==2213) return 19;
+    if (cat==2312 || cat==1223) return 20;
+    if (cat==1323 || cat==2313) return 21;
+
     return 22;
 }

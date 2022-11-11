72965b933305346f2a83b449fdedda79575358e2
Yuanchaochen <yuancc@ihep.ac.cn>
2022-11-11 17:24:02 +0800

    update

--- Analysis/Higgs2TauTauG/analysis.py
+++ Analysis/Higgs2TauTauG/analysis.py
@@ -53,8 +53,15 @@ def myanalysis(logger, h, ev, dataset, isData, histAxisName, year, xsec, sow, pa
     #-------------------------------------------------------------------------------------------------------
     # For MC
     if not isData:
-        genw = ev["genWeight"]
-        ev["weight_norm"] = (xsec / sow) * genw
+         if pass_options=='Xsecweight':
+            genw = ev["genWeight"]
+            ev["weight_norm"] = (xsec / sow) * genw
+            #ev["weight_norm"] =1
+         else:
+            ev["weight_norm"]=1
+    
+   
+    
     #-------------------------------------------------------------------------------------------------------
     # Masks
     #-------------------------------------------------------------------------------------------------------
@@ -64,7 +71,30 @@ def myanalysis(logger, h, ev, dataset, isData, histAxisName, year, xsec, sow, pa
     #-------------------------------------------------------------------------------------------------------
     # Fill histograms
     hists.fill('ElectronPt',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.lightlepton[:,0], ElectronPt='pt')
+    hists.fill('Electroneta',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.lightlepton[:,0], Electroneta='eta')
+    hists.fill('Electronphi',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.lightlepton[:,0], Electronphi='phi')
+    hists.fill('MuonPt',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.lightlepton[:,0], MuonPt='pt')
+    hists.fill('Muoneta',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.lightlepton[:,0], Muoneta='eta')
+    hists.fill('Muonphi',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.lightlepton[:,0], Muonphi='phi')
+    hists.fill('TauPt_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.mytau[:,0], TauPt_eh='pt')
+    hists.fill('Taueta_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.mytau[:,0], Taueta_eh='eta')
+    hists.fill('Tauphi_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.mytau[:,0], Tauphi_eh='phi')
+    hists.fill('TauPt_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.mytau[:,0], TauPt_uh='pt')
+    hists.fill('Taueta_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.mytau[:,0], Taueta_uh='eta')
+    hists.fill('Tauphi_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.mytau[:,0], Tauphi_uh='phi')
 
+    hists.fill('PhotonPt_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.myphoton[:,0], PhotonPt_eh='pt')
+    hists.fill('Photoneta_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.myphoton[:,0], Photoneta_eh='eta')
+    hists.fill('Photonphi_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev.myphoton[:,0], Photonphi_eh='phi')
+    hists.fill('PhotonPt_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.myphoton[:,0], PhotonPt_uh='pt')
+    hists.fill('Photoneta_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.myphoton[:,0], Photoneta_uh='eta')
+    hists.fill('Photonphi_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev.myphoton[:,0], Photonphi_uh='phi')
+
+    hists.fill('dr_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev, dr_eh='dr')
+    hists.fill('dr_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev, dr_uh='dr')
+    hists.fill('invarmass_eh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==11), ev, invarmass_eh='invarmass')
+    hists.fill('invarmass_uh',ev.weight_norm, (abs(ev.lightlepton[:,0].pdgId) ==13), ev, invarmass_uh='invarmass')
+    hists.fill('MET',ev.weight_norm, ((abs(ev.lightlepton[:,0].pdgId) ==13)|(abs(ev.lightlepton[:,0].pdgId) ==11)),ev.MET, MET='pt')
     #-------------------------------------------------------------------------------------------------------
     #-------------------------------------------------------------------------------------------------------
     #End your analysis
@@ -75,5 +105,32 @@ def myanalysis(logger, h, ev, dataset, isData, histAxisName, year, xsec, sow, pa
 def binning(a,b,c):
     return list(np.arange(a,b+c,c))
 histograms = {
-    'ElectronPt':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('ElectronPt', '$ElectronPt$', binning(0,50,1)))
+    'ElectronPt':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('ElectronPt', '$ElectronPt$', binning(20,100,1))),
+    'MuonPt':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('MuonPt', '$MuonPt$', binning(20,100,1))),
+    'TauPt_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('TauPt_eh', '$TauPt_eh$', binning(30,100,1))),
+    'TauPt_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('TauPt_uh', '$TauPt_uh$', binning(30,100,1))),
+
+    'Electroneta':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Electroneta', '$Electroneta$', binning(-4,4,1))),
+    'Muoneta':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Muoneta', '$Muoneta$', binning(-4,4,1))),
+    'Taueta_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Taueta_eh', '$Taueta_eh$', binning(-4,4,1))),
+    'Taueta_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Taueta_uh', '$Taueta_uh$', binning(-4,4,1))),
+
+    'Electronphi':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Electronphi', '$Electronphi$', binning(-4,4,1))),
+    'Muonphi':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Muonphi', '$Muonphi$', binning(-4,4,1))),
+    'Tauphi_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Tauphi_eh', '$Tauphi_eh$', binning(-4,4,1))),
+    'Tauphi_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Tauphi_uh', '$Tauphi_uh$', binning(-4,4,1))),
+
+    'dr_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('dr_eh', '$dr_eh$', binning(0,10,1))),
+    'dr_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('dr_uh', '$dr_uh$', binning(0,10,1))),
+    'invarmass_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('invarmass_eh', '$invarmass_eh$', binning(0,70,1))),
+    'invarmass_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('invarmass_uh', '$invarmass_uh$', binning(0,70,1))),
+
+    'MET':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('MET', '$MET$', binning(0,100,1))),
+    'PhotonPt_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('PhotonPt_eh', '$PhotonPt_eh$', binning(4,50,1))),
+    'Photoneta_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Photoneta_eh', '$Photoneta_eh$', binning(-4,4,1))),
+    'Photonphi_eh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Photonphi_eh', '$Photonphi_eh$', binning(-4,4,1))),
+    'PhotonPt_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('PhotonPt_uh', '$PhotonPt_uh$', binning(4,50,1))),
+    'Photoneta_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Photoneta_uh', '$Photoneta_uh$', binning(-4,4,1))),
+    'Photonphi_uh':hist.Hist('Events',hist.Cat('process', 'process'),hist.Bin('Photonphi_uh', '$Photonphi_uh$', binning(-4,4,1)))
+
 }
--- Analysis/Higgs2TauTauG/preprocessor_ehuh.py
+++ Analysis/Higgs2TauTauG/preprocessor_ehuh.py
@@ -27,29 +27,35 @@ def preprocess(sample,events,AttachSF=True):
     events["ttgevents"]=(ak.num(events.photonswithcut)==1)
 
 
-    events["mytau"]=events.Tau[(events.Tau.idDecayModeNewDMs==True) & (events.Tau.pt > 30) & (events.Tau.pt <45) & (abs(events.Tau.eta)<2.4)]
+    #events["mytau"]=events.Tau[(events.Tau.idDecayModeNewDMs==True) & (events.Tau.pt > 30) & (events.Tau.pt <45) & (abs(events.Tau.eta)<2.4)]
+    events["mytau"]=events.Tau[(events.Tau.idDecayModeNewDMs==True) & (events.Tau.pt > 30) &  (abs(events.Tau.eta)<2.4)]
     events["myelectron"]=events.Electron[(events.Electron.cutBased >=3) & (events.Electron.pt > 24) & (abs(events.Electron.eta)<2.1)]
     events["myphoton"]=events.Photon[(events.Photon.cutBased >=2) & (events.Photon.pt > 5) & (abs(events.Photon.eta)<2.5)]
     events["mymuon"]=events.Muon[(events.Muon.mediumId==True) & (events.Muon.pt > 20) & (abs(events.Muon.eta)<2.1)]
     events["lightlepton"]=ak.with_name(ak.concatenate([events.myelectron, events.mymuon], axis=1), 'PtEtaPhiMCandidate')
     if histAxisName=="H2TTTG":
-
-        events=events[(events.ttgevents==1) & (ak.num(events.mytau)==1) & (ak.num(events.myphoton)==1) & ak.num(events.lightlepton)==1]
+        events=events[(events.ttgevents==1)]
     elif histAxisName=="H2TTT":
-        events=events[(events.ttgevents==0) & (ak.num(events.mytau)==1) & (ak.num(events.myphoton)==1) & ak.num(events.lightlepton)==1]
+        events=events[(events.ttgevents==0)]
     else:
-        events=events[(ak.num(events.mytau)==1) & (ak.num(events.myphoton)==1) & ak.num(events.lightlepton)==1]
+        events=events
+    
+    events=events[ak.num(events.lightlepton)==1]
+    events["drlg"]=events.myphoton.delta_r(events.lightlepton[:,0])
+    events.myphoton=events.myphoton[events.drlg > 0.4]
+    events=events[(ak.num(events.mytau)==1) & (ak.num(events.myphoton)==1)]
+
     ev_tau=events.mytau[:,0]
     ev_lepton=events.lightlepton[:,0]
     ev_photon=events.myphoton[:,0]
 
-    events["ehobject"]=ev_tau+ev_lepton
-    events["ehgobject"]=ev_tau+ev_lepton+ev_photon
+    events["lhobject"]=ev_tau+ev_lepton
+    events["lhgobject"]=ev_tau+ev_lepton+ev_photon
 
-    events["dreh"]=ev_lepton.delta_r(ev_tau)
+    events["dr"]=ev_lepton.delta_r(ev_tau)
     events["chargeeh"]=ev_lepton.charge*ev_tau.charge
-    events["invarmass"]=events.ehobject.mass
-    events["Mteh"]=np.sqrt(2*ev_lepton.pt*ev_tau.pt*(1-np.cos(ev_lepton.delta_phi(ev_tau))))
+    events["invarmass"]=events.lhobject.mass
+    #events["Mteh"]=np.sqrt(2*ev_lepton.pt*ev_tau.pt*(1-np.cos(ev_lepton.delta_phi(ev_tau))))
     # Compute pair invariant masses, for all flavors all signes
 
     return events,dataset,isData,histAxisName,year,xsec,sow

3b5aa756fee1c4fc8d8130effa985a7e0786adae
akapoorcern <akapoor@cern.ch>
2022-11-19 22:59:09 +0100

    new setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
+echo "########### Setting up env for expresso ##############"
+
+system=$(uname --all)
+if [[ "$system" == *"WSL2"* ]]; then
+   echo "In wsl2"
+   conda activate py37_coffea_hep
+elif [[ "$system" == *"lxslc"* ]]; then
+   conda activate expresso
+elif [[ "$system" == *"lxplus"* ]]; then
+    conda activate expresso
+else
+    echo "Are you in supported node?"
+fi
+   
+      
+
 chmod +x plot+.py
 chmod +x expresso.py
 pip install -e .
 chmod +x modules/createJSON.py
 
-alias createJSON="python modules/createJSON.py"
 
 ehelp () {
     python expresso.py --help

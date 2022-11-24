a6815dcd7c536367fba32c84c9e8cd294f2dff1d
Yuanchaochen <yuancc@ihep.ac.cn>
2022-11-25 01:34:31 +0800

    cut config

---
+++ modules/cut.py
@@ -0,0 +1,45 @@
+import modules.cut_configure as cut
+def isPresTau(pt, eta, dxy, dz, idDeepTau2017v2p1VSjet, minpt=20.0):
+    return  (pt>minpt)&(abs(eta)< cut.eta_tau_cut )&(abs(dxy)<cut.dxy_tau_cut)&(abs(dz)<cut.dz_tau_cut) & (idDeepTau2017v2p1VSjet>>1 & 1 ==1)
+
+def isTightTau(idDeepTau2017v2p1VSjet):
+    return (idDeepTau2017v2p1VSjet>>2 & 1 ==1)
+
+def ismedTau(idDeepTau2017v2p1VSjet):
+    return (idDeepTau2017v2p1VSjet>>2 & 1 ==1)
+
+def islooseTau(idDeepTau2017v2p1VSjet):
+    return (idDeepTau2017v2p1VSjet>>2 & 1 ==1)
+
+def istightElectron(pt,eta,cutBased, minpt=24.0):
+    return (pt>minpt) & (abs(eta)<cut.eta_electron_cut) & (cutBased >= 4)
+
+def ismedElectron(pt,eta,cutBased, minpt=24.0):
+    return (pt>minpt) & (abs(eta)<cut.eta_electron_cut) & (cutBased >= 3)
+
+def islooseElectron(pt,eta,cutBased, minpt=24.0):
+    return (pt>minpt) & (abs(eta)<cut.eta_electron_cut) & (cutBased >= 2)
+
+def istightMuon(pt,eta,cutBased, minpt=20.0):
+    return (pt>minpt) & (abs(eta)<cut.eta_muon_cut) & (cutBased ==True)
+
+def ismedMuon(pt,eta,cutBased, minpt=20.0):
+    return (pt>minpt) & (abs(eta)<cut.eta_muon_cut) & (cutBased ==True)
+
+def islooseMuon(pt,eta,cutBased, minpt=20.0):
+    return (pt>minpt) & (abs(eta)<cut.eta_muon_cut) & (cutBased ==True)
+
+def istightPhoton(pt,eta,cutBased, minpt=5):
+    return (pt>minpt) & (abs(eta)<cut.eta_photon_cut) & (cutBased >= 3)
+
+def ismedPhoton(pt,eta,cutBased, minpt=5):
+    return (pt>minpt) & (abs(eta)<cut.eta_photon_cut) & (cutBased >= 2)
+
+def isloosePhoton(pt,eta,cutBased, minpt=5):
+    return (pt>minpt) & (abs(eta)<cut.eta_photon_cut) & (cutBased >= 1)
+
+
+
+
+
+
---
+++ modules/cut_configure.py
@@ -0,0 +1,6 @@
+eta_tau_cut= 2.4
+dxy_tau_cut= 999
+dz_tau_cut= 0.4
+eta_electron_cut=2.1
+eta_muon_cut=2.4
+eta_photon_cut=2.4

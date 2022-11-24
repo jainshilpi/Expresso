7ceaad6695579caf129d9b0038d927ecfceff08a
akapoorcern <akapoor@cern.ch>
2022-11-24 19:43:33 +0100

    run.sh for higgs

--- Analysis/Higgs2TauTauG/run.sh
+++ Analysis/Higgs2TauTauG/run.sh
@@ -1,8 +1,11 @@
 
-d=$3
-if [ $d == '1' ]; then
-    python expresso.py --Sample $1  --NumberOfTasks $2 --Analysis Higgs2TauTauG  --PreProcessor Analysis/Higgs2TauTauG/preprocessor_ehuh.py  --PreSelector Analysis/Higgs2TauTauG/preselection_ehuh.py --AnalysisScript Analysis/Higgs2TauTauG/analysis.py --Debug --PassOptions $4 --SaveRoot
+# d=$3
+# if [ $d == '1' ]; then
+#     python expresso.py --Sample $1  --NumberOfTasks $2 --Analysis Higgs2TauTauG  --PreProcessor Analysis/Higgs2TauTauG/preprocessor_ehuh.py  --PreSelector Analysis/Higgs2TauTauG/preselection_ehuh.py --AnalysisScript Analysis/Higgs2TauTauG/analysis.py --Debug --PassOptions $4 --SaveRoot
     
-else
-    python expresso.py --Sample $1  --NumberOfTasks $2 --Analysis Higgs2TauTauG  --PreProcessor Analysis/Higgs2TauTauG/preprocessor_ehuh.py  --PreSelector Analysis/Higgs2TauTauG/preselection_ehuh.py --AnalysisScript Analysis/Higgs2TauTauG/analysis.py --PassOptions $4 --SaveRoot
-fi
+# else
+#     python expresso.py --Sample $1  --NumberOfTasks $2 --Analysis Higgs2TauTauG  --PreProcessor Analysis/Higgs2TauTauG/preprocessor_ehuh.py  --PreSelector Analysis/Higgs2TauTauG/preselection_ehuh.py --AnalysisScript Analysis/Higgs2TauTauG/analysis.py --PassOptions $4 --SaveRoot
+# fi
+
+
+#./expresso.py --Samples Analysis/testAnalysis/samples.txt --Analysis Higgs2TauTauG --NumberOfTasks 1 --AnalysisPoint tight_tau_tight_l_tight_photon
